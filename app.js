ab7cf75ddcda94e28b9f52e37f59a5eb8f6169c0
anushkaindunil1 <anushkaindunil157@gmail.com>
2024-06-19 11:04:22 +0530

    calculator

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+function calc() {
+    
+    let num1 = document.getElementById("number 1").value;
+    let num2 = document.getElementById("number 2").value;
+    let op1 = document.getElementById("op").value;
+    let lblOutput = document.getElementById("output");
+
+    lblOutput.innerHTML = num1+num2;
+
+}
+
+
