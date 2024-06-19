1ee3903af0b25f9f97ca9f8f59717e1b0343e6ab
anushkaindunil1 <anushkaindunil157@gmail.com>
2024-06-19 11:17:05 +0530

    update a styles

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
+const displayElement = document.getElementById('display');
 
-
-
-
-
-function calc() {
-    
-    let num1 = document.getElementById("number 1").value;
-    let num2 = document.getElementById("number 2").value;
-    let op1 = document.getElementById("op").value;
-    let lblOutput = document.getElementById("output");
-
-    lblOutput.innerHTML = num1+num2;
-
+function clearDisplay() {
+    displayElement.innerText = '0';
 }
 
+function deleteLast() {
+    if (displayElement.innerText.length === 1) {
+        displayElement.innerText = '0';
+    } else {
+        displayElement.innerText = displayElement.innerText.slice(0, -1);
+    }
+}
 
+function appendCharacter(character) {
+    if (displayElement.innerText === '0') {
+        displayElement.innerText = character;
+    } else {
+        displayElement.innerText += character;
+    }
+}
+
+function calculateResult() {
+    try {
+        displayElement.innerText = eval(displayElement.innerText);
+    } catch {
+        displayElement.innerText = 'Error';
+    }
+}
